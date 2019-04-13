852c0ad690a9a69f78569978e694f061a9fbc0fa
Jessica Minnier <jessica.minnier@gmail.com>
2019-04-12 17:40:26 -0700

    add some files

---
+++ pre_class_homework.R
@@ -0,0 +1,26 @@
+
+# make sure you have the most recent versions of the packages
+install.packages(c("tidyverse","tidyr","readr","readxl","forcats","glue","janitor"))
+
+library(tidyverse)
+library(lubridate)
+library(readxl)
+library(janitor)
+
+# load the data set named iris
+data(iris)
+
+# how many rows and columns does iris have?
+
+# print a summary of iris
+
+# what is the sum of all sepal length's in iris?
+
+# create a data frame that is a subset of the iris data set with only setosa species
+
+# import the csv file in the data/ folder called "small_data.csv", call it mydata
+
+# print a summary of mydata
+
+# close Rstudio and reopen this project
+
