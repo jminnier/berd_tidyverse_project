7ce5b8df04ef78a9ebd349a997baec4874096efa
Jessica Minnier <jessica.minnier@gmail.com>
2019-04-15 17:12:11 -0700

    update answers

--- pre_class_homework_answers.R
+++ pre_class_homework_answers.R
@@ -11,16 +11,27 @@ library(janitor)
 data(iris)
 
 # how many rows and columns does iris have?
+dim(iris)
+nrow(iris)
+ncol(iris)
 
 # print a summary of iris
+summary(iris)
 
 # what is the sum of all sepal length's in iris?
+sum(iris$Sepal.Length)
 
 # create a data frame that is a subset of the iris data set with only setosa species
+setosa_data <- iris[iris$Species=="setosa",]
 
 # import the csv file in the "data/" folder called "small_data.csv", call it mydata
+mydata <- read.csv("data/small_data.csv")
+# or
+mydata <- read_csv("data/small_data.csv")
 
 # print a summary of mydata
+summary(mydata)
 
 # close Rstudio and reopen this project
+# answer: just open the folder and open/double click on the berd_tidyverse_project.Rproj file
 
